6d2feaca6de12163eced077fe8c72e142ac145b2
zhwei <zhwei.yes@gmail.com>
2020-12-30 20:20:45 +0800

    fix tests

--- tests/Feature/TeacherApiTest.php
+++ tests/Feature/TeacherApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\School;
 use App\Student;
 use App\Teacher;
 use Carbon\Carbon;
@@ -16,12 +17,16 @@ class TeacherApiTest extends TestCase
 
     public function testListFollowing()
     {
+        $school = factory(School::class)->create();
         $teacher = factory(Teacher::class)->create();
-        $students = factory(Student::class)->times(3)->create();
+        $students = factory(Student::class)->times(3)->create([
+            'student_school_id' => $school->id,
+        ]);
         $teacher->following_students()->attach($students, ['created_at' => Carbon::now()]);
 
         Passport::actingAs($teacher);
         $resp = $this->get('/teachers/students/following');
+        throw_if($resp->exception, $resp->exception);
         $resp->assertSuccessful();
         self::assertSame($students->pluck('id')->all(), Arr::pluck($resp->json()['items'], 'id'));
     }
